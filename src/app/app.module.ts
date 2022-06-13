d44b29eebeb5ebbcc5d2f45b0948b4ac0ed9e47b
xxavierargentino <xxavierargentino@hotmail.com>
2022-06-13 08:54:34 -0300

    aplicacion completada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProtegidaComponent,
+    PreciosComponent,
+    HomeComponent
   ],
   imports: [
+    AuthModule.forRoot({
+      domain: "dev-igw2fl07.us.auth0.com",
+      clientId: "PQeoB2jQeoFDJ34CwtuF8IvMptmeWamk",
+      
+    }),
     BrowserModule,
     AppRoutingModule
   ],
